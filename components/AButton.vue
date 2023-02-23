eab5647074ee64ffe6da380f7d527170d2d1cb71
Kashirskiy Nikita <lordwebkit@gmail.com>
2023-02-23 20:44:58 +0300

    Created base button component

---
+++ components/AButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="button">
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  name: "AButton",
+};
+</script>
+<style lang="scss" scoped>
+@import "~/assets/styles/color";
+.button {
+  transition: all 0.2s ease;
+  padding: 18px 26px;
+  background-color: transparent;
+  color: $corporate-blue;
+  border: 1px solid $corporate-blue;
+  cursor: pointer;
+  &:hover {
+    background-color: $corporate-blue;
+    color: #fff;
+  }
+}
+</style>
