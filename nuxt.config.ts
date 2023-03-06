92046cc91b983b357dfcc08539cd568e1151fbe4
Kashirskiy Nikita <lordwebkit@gmail.com>
2023-03-06 21:52:29 +0300

    Added favicon for all devices

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,14 @@ export default defineNuxtConfig({
           content:
             "Двери оптом купить от официального производителя АкваДверь в России. Доставка по всем регионам.",
         },
+        {
+          name: "msapplication-TileColor",
+          content: "#2d89ef",
+        },
+        {
+          name: "theme-color",
+          content: "#ffffff",
+        },
       ],
       link: [
         {
@@ -20,6 +28,27 @@ export default defineNuxtConfig({
         {
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest",
         },
       ],
     },
